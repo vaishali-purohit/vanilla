e54ac5fe840ae6c9565e81833914358c06bb1b95
Vaishali Purohit <vaishalipurohit12@gmail.com>
2023-01-18 21:57:21 +0530

    Fix for glitch

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const userConfig = require("./configs/user");
 
-const env = process.env.NODE_ENV ?? "development";
+const env = process.env.NODE_ENV ? process.env.NODE_ENV : "development";
 const config = userConfig[env];
 
 const database = config.database;
